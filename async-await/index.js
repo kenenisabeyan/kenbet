0cb3f96c0386763deda55ae4211ef1854202bda2
Kenenisa Beyan <kenenisab05@gmail.com>
2026-03-12 11:29:45 +0200

    addd this on to the callback functions

---
+++ async-await/index.js
@@ -0,0 +1,26 @@
+function delayFn(time){
+  return new Promise((resolve)=> setTimeout)
+}
+
+async function delayedGreet(name){
+  await delayFn(2000)
+  console.log(name);
+}
+
+delayedGreet("Kenenisa Beyan");
+
+async function division(num1, num2){
+  try{
+    if(num2 === 0) throw new Error('Can not divide by 0')
+      return num1/num2;
+
+  }catch(error){
+    console.error('error', error)
+    return null
+  }
+}
+
+async function mainFn(){
+  console.log(await division(10, 2))
+  console.log(await devision(10, 0))
+}
