4f7ba4125fafcccfd95a6c7bafc6f2556396d088
Kenenisa Beyan <kenenisab05@gmail.com>
2026-03-13 18:07:15 +0200

    feat(ejs): configure ejs view engine and define initial products data

--- ejs-tutorial/index.js
+++ ejs-tutorial/index.js
@@ -7,6 +7,26 @@ const path = express();
 
   app.set("views", path.join(__dirname, "views"));
 
+  const products = [
+    { name: "Product 1", price: 10 },
+    { name: "Product 2", price: 20 },
+    { name: "Product 3", price: 30 },
+  ];  
+
   app.get("/", (req, res) => {
     res.render("index", { title: "Home Page" });
   });
+
+  app.get("/products", (req, res) => {
+    res.render("products", { title: "Products Page", products });
+  });
+
+  const PORT = 3000;    
+
+  app.get("/about", (req, res) => {
+    res.render("about", { title: "About Us" });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });   
