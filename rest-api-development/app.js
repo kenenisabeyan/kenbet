5f9ee94de16ca8c162e06f30c4a4ec14d48b9b8e
Kenenisa Beyan <kenenisab05@gmail.com>
2026-03-13 18:26:43 +0200

    feat(api): implement initial books data and retrieval endpoints

--- rest-api-development/app.js
+++ rest-api-development/app.js
@@ -7,3 +7,23 @@ let books = [
   { id: 1, title: "Book One", author: "Author One" },
   { id: 2, title: "Book Two", author: "Author Two" },
 ];  
+
+app.get("/books", (req, res) => {
+  res.json({
+    message: "Books retrieved successfully",
+    data: books,
+  });
+});
+
+app.get('/get' , (req, res) => {
+  res.json({
+    message: "Books retrieved successfully",
+    data: books,
+  });
+});   
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
