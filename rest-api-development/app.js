c99cef250ec6a8bc6218c05cde07b81691b3f5e0
Kenenisa Beyan <kenenisab05@gmail.com>
2026-03-14 08:33:28 +0200

    chore: setup nodemon and initial mock-CRUD logic
    - Added nodemon as a dev dependency for hot-reloading
    - Implemented basic GET and POST routes for personal goals
    - Configured JSON middleware for incoming request parsing

--- rest-api-development/app.js
+++ rest-api-development/app.js
@@ -27,3 +27,20 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
+
+app.get('/get' , (req, res) => {
+  res.json({
+    message: "Books retrieved successfully",
+    data: books,
+  });
+});
+
+app.post("/books", (req, res) => {
+  const { title, author } = req.body;
+  const newBook = { id: books.length + 1, title, author };
+  books.push(newBook);
+  res.status(201).json({
+    message: "Book created successfully",
+    data: newBook,
+  });
+} );    
