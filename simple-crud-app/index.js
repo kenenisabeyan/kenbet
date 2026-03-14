0fd49d1c76957697f627ae6ed64030e1c378bda6
Kenenisa Beyan <kenenisab05@gmail.com>
2026-03-14 08:27:22 +0200

    chore: setup nodemon and initial mock-CRUD logic
    - Added nodemon as a dev dependency for hot-reloading
    - Implemented basic GET and POST routes for personal goals
    - Configured JSON middleware for incoming request parsing

--- simple-crud-app/index.js
+++ simple-crud-app/index.js
@@ -1,1 +1,13 @@
-console.log("Hello there");
+const express = require('express')
+const app = express()
+
+app.listen(3000, () =>{
+  console.log("Server is running on port 3000");
+
+});
+
+
+app.get('/', (req, res) =>{
+  res.send("Hello fron node API server")
+
+});
